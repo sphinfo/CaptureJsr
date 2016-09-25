590ad0f181f850e8d8717e1ebbf767e4023117d3
Administrator <Administrator@JMKIM-PC>
2016-09-26 00:20:43 +0900

    버전 수정

--- src/main/java/com/sphinfo/sample/PrintJsr.java
+++ src/main/java/com/sphinfo/sample/PrintJsr.java
@@ -34,7 +34,7 @@ public class PrintJsr extends JaxAlgorithResultSetResource<JSONObject> {
 	@Override
 	protected String getAlgorithmName() {
 		// TODO Auto-generated method stub
-		return "capture";
+		return "print";
 	}
 	
 	// 실행 결과
@@ -63,8 +63,8 @@ public class PrintJsr extends JaxAlgorithResultSetResource<JSONObject> {
 			String width = json.getString("width") == null ? "" : json.getString("width");
 			String height = json.getString("height") == null ? "" : json.getString("height");
 			
-			makeHtml(body, htmlFileNm);
-			runCapture(width, height, htmlFileNm, imageFileNm);
+			SphUtils.makeHtml(body, htmlFileNm);
+			SphUtils.runCapture(width, height, htmlFileNm, imageFileNm);
 			
 			resultCode = "200";
 			result.put("code",resultCode);
@@ -82,77 +82,12 @@ public class PrintJsr extends JaxAlgorithResultSetResource<JSONObject> {
 			return result.toString();
 		} finally {
 			
-//			File htmlFile = new File(htmlFileNm);
-//			if(htmlFile.exists()){
-//				htmlFile.delete();
-//			}
+			File htmlFile = new File(htmlFileNm);
+			if(htmlFile.exists()){
+				htmlFile.delete();
+			}
 		}
 		
-	}
-
-	private void makeHtml(String body, String htmlFileNm) throws Exception{
-		FileWriter fw = null;
-		try{
-			body = body.replaceAll("%26amp;", "&");
-			StringBuffer html = new StringBuffer();
-			html.append("<html>\n");
-			html.append("<head><meta charset=\"EUC-KR\"/></head>\n");
-			html.append("<body>\n");
-			html.append(body);
-			// html.append("<div style='width:100%;height:100%;background-color:#FEF112;'/>\n");
-			html.append("\n</body>\n");
-			html.append("</html>");
-			
-			fw = new FileWriter(htmlFileNm);
-			fw.write(html.toString());
-			fw.flush();
-			fw.close();
-		}finally{
-			if (fw != null) {
-				try {
-					fw.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-	private void runCapture(String width,String height,String htmlFileNm,String imageFileNm) throws Exception{
-		Runtime run = Runtime.getRuntime();
-		Process p = null;
-		BufferedReader br = null;
-		try{
-			String pWidth = "--width " + width;
-			String pHeight = "--height " + height;
-			String cmdarray = "wkhtmltoimage" + " " + pWidth + " " + pHeight
-					+ " " + htmlFileNm + " " + imageFileNm;
-			System.out.println(cmdarray);
-			p = run.exec(cmdarray);
-
-			br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String line = null;
-			while ((line = br.readLine()) != null) {
-				System.out.println(line);
-			}
-			// 프로세스의 수행이 끝날때까지 대기
-			p.waitFor();
-			br.close();
-			p.destroy();
-		}  finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-
-			if (p != null) {
-				p.destroy();
-			}
-		}
 	}
 
 }
