01be034ae5638ee8c694d5e9bdab5db009a0bc01
Administrator <Administrator@JMKIM-PC>
2016-09-26 00:19:22 +0900

    test

--- src/main/java/com/sphinfo/sample/SphUtils.java
+++ src/main/java/com/sphinfo/sample/SphUtils.java
@@ -7,6 +7,18 @@ import java.io.InputStreamReader;
 
 public class SphUtils {
 
+	/**
+	 * 
+	* <pre>
+	* 1. 메소드명 : makeHtml
+	* 2. 작성일 : 2016. 9. 25. 오후 11:44:08
+	* 3. 작성자 : Administrator
+	* 4. 설명 :
+	* </pre>
+	* @param body
+	* @param htmlFileNm
+	* @throws Exception
+	 */
 	public static void makeHtml(String body, String htmlFileNm) throws Exception{
 		FileWriter fw = null;
 		try{
@@ -35,6 +47,36 @@ public class SphUtils {
 			}
 		}
 	}
+	
+	public static void makePdfHtml(String path, String pdfSize, String pdfOrientation) throws Exception{
+		FileWriter fw = null;
+		try{
+			StringBuffer html = new StringBuffer();
+			html.append("<html>\n");
+			html.append("<head><meta charset=\"EUC-KR\"/></head>\n");
+			html.append("<body>\n");
+			html.append("<img src='").append(path);
+			html.append("' ");
+			html.append("/>");
+			// html.append("<div style='width:100%;height:100%;background-color:#FEF112;'/>\n");
+			html.append("\n</body>\n");
+			html.append("</html>");
+			
+			fw = new FileWriter(path);
+			fw.write(html.toString());
+			fw.flush();
+			fw.close();
+		}finally{
+			if (fw != null) {
+				try {
+					fw.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 	public static  void runCapture(String width,String height,String htmlFileNm,String imageFileNm) throws Exception{
 		Runtime run = Runtime.getRuntime();
 		Process p = null;
@@ -47,6 +89,45 @@ public class SphUtils {
 			System.out.println(cmdarray);
 			p = run.exec(cmdarray);
 
+			br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+			// 프로세스의 수행이 끝날때까지 대기
+			p.waitFor();
+			br.close();
+			p.destroy();
+		}  finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+			if (p != null) {
+				p.destroy();
+			}
+		}
+	}
+	
+	public static  void runPdf(String width, String height, String htmlFileNm,String imageFileNm) throws Exception{
+		Runtime run = Runtime.getRuntime();
+		Process p = null;
+		BufferedReader br = null;
+		try{
+			String pWidth = "--width " + width;
+			String pHeight = "--height " + height;
+			String cmdarray = "wkhtmltopdf" + " " + pWidth + " " + pHeight
+					+ " " + htmlFileNm + " " + imageFileNm;
+			System.out.println(cmdarray);
+			p = run.exec(cmdarray);
+			
+			
+			
 			br = new BufferedReader(new InputStreamReader(p.getInputStream()));
 			String line = null;
 			while ((line = br.readLine()) != null) {
